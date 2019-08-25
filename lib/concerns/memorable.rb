2e7f409376d8099b7b8e16a32b583963f7f7a076
jose luis <cortezluisjose@gmail.com>
2019-08-25 03:51:55 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,4 @@
  require_relative '../lib/concerns/memorable'
  
  
- module Memorable 
-   module ClassMethods
-   
-  def self.reset_all 
-    self.all.clear
-    
-  end 
-  def self.count 
-    self.all.count 
-    
-  end
-  
-  
-  
-end
+ 
